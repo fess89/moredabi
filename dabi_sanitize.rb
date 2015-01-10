9651b81f27aa52e601e7ffc64109841fe6b0ea7b
Oleksii Chyrkov <fess89@gmail.com>
2015-01-10 15:09:02 +0200

    Create dabi_sanitize.rb
    Sanitizing the data removing the non-ASCII whitespaces.

---
+++ dabi_sanitize.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+input = "test.csv"
+output = "output.csv"
+
+class String
+
+	def gsub_or_self!(x,y)
+		gsub!(x,y) || self
+	end
+
+  def strip_or_self!
+    strip! || self
+  end
+
+end
+
+CSV.open(output, "w") do |writer|
+	CSV.foreach(input, converters: lambda {|s| s ? s.gsub_or_self!(/[[:space:]]$/,"").strip_or_self! : nil}) do |row|
+		writer << row		
+	end
+end
